6ff201e59d43c9ba2226835899bb2915cfcfa279
Talha <talha.b.future@gmail.com>
2022-03-23 16:57:59 +0300

    the protoype works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
 import _ from 'lodash';
 
 
-console.log(`this is a test to check if the file is ported to main.js`)
+const title = (titleName)  => {  
+    return {titleName}
+}
+
+const description = (descriptionName)  => { 
+    return {descriptionName }
+}
+const date = (dateName)  => { 
+    return {dateName }
+}
+const priority = (priorityName)  => { 
+    return {priorityName };
+}
+
+// const dinner = title(`dinner`);
+// console.log(dinner.titleName)
+
+
+const task = (titlePut,descriptionPut,datePut,priorityPut) => {
+    // const assginInfos = (titlePut) => {
+        
+    // let titleOfTask = title(titlePut);
+    // return titleOfTask
+    // }
+
+    let titleOfTask = title(titlePut);
+    let descriptionOfTask = description(descriptionPut)
+    let dateOfTask = date(datePut)
+    let priorityOfTask = priority(priorityPut)
+
+    return { titleOfTask, descriptionOfTask, dateOfTask, priorityOfTask}
+}
+
+
+let test = prompt("title");
+let test2 = prompt("desc");
+let test3 = prompt("date");
+let test4 = prompt("priority");
+const testTask = task(test,test2,test3,test4) ;
+
+console.log(testTask)
+console.log(testTask.titleOfTask)
+console.log(testTask.descriptionOfTask)
+console.log(testTask.dateOfTask)
+console.log(testTask.priorityOfTask)
+
+
