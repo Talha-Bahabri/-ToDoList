cebcdc95bc6855d88c4acefd46da87cae66b01f3
Talha <talha.b.future@gmail.com>
2022-03-26 17:29:49 +0300

    added and changed some files

---
+++ src/modules/defaultAddedProjects.js
@@ -0,0 +1,10 @@
+export default function defaultPageDom() {
+
+}
+
+function defaultProject() {
+    const project = document.createElement(`p`);
+    project.textContent = `First project`;
+
+    
+}
